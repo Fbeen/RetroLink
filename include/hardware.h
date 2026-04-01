4a2f5799d8775dddc20b4d596a9fbdd5e31d7747
Frank Beentjes <frankbeen@gmail.com>
2026-04-01 21:01:16 +0200

    new hardware version

--- include/hardware.h
+++ include/hardware.h
@@ -24,19 +24,19 @@ SFR(P3, PORT3);
 
 
 /* ST mouse aliases */
-SBIT(MX2, PORT2, 0);        // DB9 pin 1 = X2
-SBIT(MX1, PORT2, 1);        // DB9 pin 2 = X1
-SBIT(MY1, PORT2, 2);        // DB9 pin 3 = Y1
-SBIT(MY2, PORT2, 3);        // DB9 pin 4 = Y2
-SBIT(MBTN_LEFT, PORT2, 4);  // DB9 pin 6 = LEFT MOUSE BUTTON
-SBIT(MBTN_RIGHT, PORT2, 6); // DB9 pin 9 = RIGHT MOUSE BUTTON
+SBIT(MX2, PORT2, 6);        // DB9 pin 1 = X2
+SBIT(MX1, PORT2, 4);        // DB9 pin 2 = X1
+SBIT(MY1, PORT2, 3);        // DB9 pin 3 = Y1
+SBIT(MY2, PORT2, 2);        // DB9 pin 4 = Y2
+SBIT(MBTN_LEFT, PORT2, 5);  // DB9 pin 6 = LEFT MOUSE BUTTON
+SBIT(MBTN_RIGHT, PORT2, 1); // DB9 pin 9 = RIGHT MOUSE BUTTON
 
 /* ST joystick aliases */
-SBIT(JOY_UP, PORT2, 0);     // DB9 pin 1 = UP
-SBIT(JOY_DOWN,  PORT2, 1);  // DB9 pin 2 = DOWN
-SBIT(JOY_LEFT,  PORT2, 2);  // DB9 pin 3 = LEFT
-SBIT(JOY_RIGHT, PORT2, 3);  // DB9 pin 4 = RIGHT
-SBIT(JOY_FIRE,  PORT2, 4);  // DB9 pin 6 = FIRE
+SBIT(JOY_UP, PORT2, 6);     // DB9 pin 1 = UP
+SBIT(JOY_DOWN,  PORT2, 4);  // DB9 pin 2 = DOWN
+SBIT(JOY_LEFT,  PORT2, 3);  // DB9 pin 3 = LEFT
+SBIT(JOY_RIGHT, PORT2, 2);  // DB9 pin 4 = RIGHT
+SBIT(JOY_FIRE,  PORT2, 5);  // DB9 pin 6 = FIRE
 
 void hw_setup(void);
 
--- src/hardware.c
+++ src/hardware.c
@@ -2,15 +2,7 @@
 
 void hw_setup(void)
 {
-    // port setup
-    PORT_CFG = 0b00101101;
-
-    /* first all outputs safe to HIGH */
-    P2 = 0xFF;
-
-    /* force bit5 LOW */
-    P2 &= ~(1 << 5);
-
-    /* and then configure them to output */
-    P2_DIR = 0b01111111;
+    PORT_CFG |= (1 << 2);  // PORT2 open drain
+    P2 = 0xFF;             // alles HIGH-Z
+    P2_DIR = 0xFF;         // outputs enabled
 }
